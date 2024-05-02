3d58fda2cd3a3e6915dddccf1804a09842abce67
abakirh <abakirhanna@gmail.com>
2024-05-02 19:40:53 -0400

    add files needed for classifier

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from keras.models import load_model
 import numpy as np
 
@@ -6,16 +6,33 @@ import google.generativeai as genai
 import google.ai.generativelanguage as glm
 from google.api_core import retry
 import json
+import os
 
 app = Flask(__name__)
 
-@app.route('/v1')
-def test_classifier():
-    version = "v5_new_commentary"
-    label_mapping_path = "classifiers/bible_label_mapping_" + version + ".json"  # Specify the path where the label mapping JSON file is saved
+@app.route('/<version>', methods=['POST'])
+def test_classifier(version):
+    if not request.is_json:
+        return "Missing JSON in request", 400
+
+    # Extract the JSON data
+    data = request.get_json()
+    input_embedding = data.get('text_vector')
+
+    
+    label_mapping_path = os.path.join(app.root_path, 'classifiers', f'bible_label_mapping_{version}.json')
     with open(label_mapping_path, "r") as f:
         label_mapping = json.load(f)
 
-    # Load the saved model
-    model_path = "classifiers/bible_label_classifier_" + version + ".keras"  # Specify the path where the model is saved
+    model_path = os.path.join(app.root_path, 'classifiers', f'bible_label_classifier_{version}.keras')
     loaded_model = load_model(model_path)
+    
+    predictions = loaded_model.predict(np.array([input_embedding]))
+
+    # Interpret the predictions (similar to before)
+    predicted_label_index = np.argmax(predictions)
+    predicted_label = label_mapping[str(predicted_label_index)]
+
+    # Add further code to use loaded_model and label_mapping as needed
+
+    return predicted_label
