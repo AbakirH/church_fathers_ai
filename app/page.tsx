4f5c9b37ff2fcb90399747b99b0c654adae59e89
abakirh <abakirhanna@gmail.com>
2024-05-02 22:43:11 -0400

    make home page look better

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,9 @@ export default function Home() {
       </header>
       <div className="max-w-screen-lg mx-auto mt-8">
             <div className="mb-8">
-                <h2 className="text-lg font-semibold mb-2">Text Similarities:</h2>
+                <h2 className="text-lg font-semibold mb-2">
+                    <a href="/similarity" className="text-blue-500 hover:text-blue-700">Text Similarities</a>
+                </h2>
                 <div className="bg-gray-100 p-4 rounded-md">
                     <p className="text-sm text-gray-600">This page helps you explore the writings of early Christian leaders stored in the database, known as Church Fathers. It offers a way to see what these historical figures wrote and how their writings connect to the Bible.</p>
                     <p className="text-sm text-gray-600">This page gives you two different ways to see how the Bible Reference relates to the text.</p>
@@ -26,20 +28,26 @@ export default function Home() {
                 </div>
             </div>
             <div className="mb-8">
-                <h2 className="text-lg font-semibold mb-2">Search:</h2>
+                <h2 className="text-lg font-semibold mb-2">
+                    <a href="/search" className="text-blue-500 hover:text-blue-700">Search</a>
+                </h2>
                 <div className="bg-gray-100 p-4 rounded-md">
                     <p className="text-sm text-gray-600">Search through the NKJV Bible or all the writings stored on this website from the Church Fathers.</p>
                     <p className="text-sm text-gray-600">Through the use of Google's model text-embedding-004, and semantic search techniques, allows you to search the Bible for what you are searching for through the meaning of the phrase instead of finding the exact order of the phrase</p>
                 </div>
             </div>
             <div className="mb-8">
-                <h2 className="text-lg font-semibold mb-2">Classification:</h2>
+                <h2 className="text-lg font-semibold mb-2">
+                    <a href="/classification" className="text-blue-500 hover:text-blue-700">Classification</a>
+                </h2>
                 <div className="bg-gray-100 p-4 rounded-md">
                     <p className="text-sm text-gray-600">This page is to show you the 4 different models that were trained and what data was used to train them.</p>
                 </div>
             </div>
             <div className="mb-8">
-                <h2 className="text-lg font-semibold mb-2">Add Text:</h2>
+                <h2 className="text-lg font-semibold mb-2">
+                    <a href="/add-text" className="text-blue-500 hover:text-blue-700">Add Text</a>
+                </h2>
                 <div className="bg-gray-100 p-4 rounded-md">
                     <p className="text-sm text-gray-600">This page is so that you can see the magic happen, where you can add text from a church father, and see a verse be extracted. Classify the text and be able to be able to parse this text in the search page after adding it.</p>
                 </div>
