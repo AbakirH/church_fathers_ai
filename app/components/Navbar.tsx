1759e969992c763b3714b0deb23fd77ddf4b2c8d
abakirh <abakirhanna@gmail.com>
2024-05-03 00:57:47 -0400

    get rid of flowbite

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <Image src={church_fathers} alt="Divine Doctrine Logo" width="60" height="60" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Divine Doctrine</span>
                 </a>
