6b190f342c201f4b71ca0d6179273915ada40efd
abakirh <abakirhanna@gmail.com>
2024-05-02 20:40:01 -0400

    test

--- app.py
+++ app.py
@@ -10,29 +10,29 @@ app = Flask(__name__)
 @app.route('/<version>', methods=['POST'])
 def test_classifier(version):
     if not request.is_json:
-        return "Missing JSON in request", 400
+    #     return "Missing JSON in request", 400
 
-    # Extract the JSON data
-    data = request.get_json()
-    input_embedding = data.get('text_vector')
+    # # Extract the JSON data
+    # data = request.get_json()
+    # input_embedding = data.get('text_vector')
 
     
     label_mapping_path = os.path.join(app.root_path, 'classifiers', f'bible_label_mapping_{version}.json')
     with open(label_mapping_path, "r") as f:
         label_mapping = json.load(f)
 
-    model_path = os.path.join(app.root_path, 'classifiers', f'bible_label_classifier_{version}.keras')
-    loaded_model = load_model(model_path)
+    # model_path = os.path.join(app.root_path, 'classifiers', f'bible_label_classifier_{version}.keras')
+    # loaded_model = load_model(model_path)
     
-    predictions = loaded_model.predict(np.array([input_embedding]))
+    # predictions = loaded_model.predict(np.array([input_embedding]))
 
-    # Interpret the predictions (similar to before)
-    predicted_label_index = np.argmax(predictions)
-    predicted_label = label_mapping[str(predicted_label_index)]
+    # # Interpret the predictions (similar to before)
+    # predicted_label_index = np.argmax(predictions)
+    # predicted_label = label_mapping[str(predicted_label_index)]
 
     # Add further code to use loaded_model and label_mapping as needed
 
-    return predicted_label
+    return "predicted_label"
 
 @app.route('/', methods=['GET'])
 def default_endpoint():
